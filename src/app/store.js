433ce948679db950062fa68096e561dc7fe3805d
Michle99 <sammiyonyx@gmail.com>
2022-01-12 13:22:58 -0500

    first commit

---
+++ src/app/store.js
@@ -0,0 +1,11 @@
+import  { configureStore } from '@reduxjs/toolkit';
+import counterReducer from '../features/counter/counterSlice';
+
+//step 1 => npm install @reduxjs/toolkit react-redux
+//step 2 => create a redux store
+export default configureStore({
+    reducer: {
+        //step 6- Add Slice Reducers to Store
+        counter: counterReducer,
+    }
+})
